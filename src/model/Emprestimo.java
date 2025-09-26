f642e00284b9f0ada04bbdb2094bda5b980fc4a6
raypaivac123 <91889336+raypaivac123@users.noreply.github.com>
2025-09-26 17:13:21 -0300

    Atualizandoi as classes

--- src/model/Emprestimo.java
+++ src/model/Emprestimo.java
@@ -1,5 +1,61 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Emprestimo {
-    
+    private int id;
+    private int livroId;
+    private int usuarioId;
+    private LocalDate dataEmprestimo;
+    private LocalDate dataDevolucao;
+
+    public Emprestimo(int id, int livroId, int usuarioId, LocalDate dataEmprestimo, LocalDate dataDevolucao) { 
+        //Contrutor
+        this.id = id;
+        this.livroId = livroId;
+        this.usuarioId = usuarioId;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    // Getters e Setters
+    public int getId() { 
+        return id; 
+    }
+
+    public void setId(int id) { 
+        this.id = id; 
+    }
+
+    public int getLivroId() { 
+        return livroId; 
+    }
+
+    public void setLivroId(int livroId) { 
+        this.livroId = livroId; 
+    }
+
+    public int getUsuarioId() { 
+        return usuarioId; 
+    }
+
+    public void setUsuarioId(int usuarioId) { 
+        this.usuarioId = usuarioId; 
+    }
+
+    public LocalDate getDataEmprestimo() { 
+        return dataEmprestimo; 
+    }
+
+    public void setDataEmprestimo(LocalDate dataEmprestimo) { 
+        this.dataEmprestimo = dataEmprestimo; 
+    }
+
+    public LocalDate getDataDevolucao() { 
+        return dataDevolucao; 
+    }
+
+    public void setDataDevolucao(LocalDate dataDevolucao) { 
+        this.dataDevolucao = dataDevolucao; 
+    }
 }
--- src/model/Livro.java
+++ src/model/Livro.java
@@ -1,5 +1,63 @@
 package model;
 
 public class Livro {
-    
+    private int id;
+    private String titulo;
+    private String autor;
+    private int ano;
+    private boolean disponivel;
+
+    public Livro(int id, String titulo, String autor, int ano, boolean disponivel) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.disponivel = disponivel;
+    }
+
+    // Getters e Setters
+    public int getId() { 
+        return id; 
+    }
+
+    public void setId(int id) { 
+        this.id = id; 
+    }
+
+    public String getTitulo() { 
+        return titulo; 
+    }
+
+    public void setTitulo(String titulo) { 
+        this.titulo = titulo; 
+    }
+
+    public String getAutor() { 
+        return autor; 
+    }
+
+    public void setAutor(String autor) { 
+        this.autor = autor; 
+    }
+
+    public int getAno() { 
+        return ano; 
+    }
+
+    public void setAno(int ano) { 
+        this.ano = ano; 
+    }
+
+    public boolean isDisponivel() { 
+        return disponivel; 
+    }
+
+    public void setDisponivel(boolean disponivel) { 
+        this.disponivel = disponivel; 
+    }
+
+    @Override
+    public String toString() {
+        return titulo + " - " + autor + " (" + ano + ")";
+    }
 }
